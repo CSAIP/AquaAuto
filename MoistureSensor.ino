22db01ac217146f12a9ddee16b294b366326f866
CSAIP <67184172+CSAIP@users.noreply.github.com>
2020-07-03 20:59:29 +0400

    try to use moisture sensor

---
+++ MoistureSensor.ino
@@ -0,0 +1,32 @@
+
+int sensorPin = A0;  
+int sensorValue = 0;  
+int percent = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  sensorValue = analogRead(sensorPin);
+  percent = convertToPercent(sensorValue);
+  printValues();
+  delay(1000);
+}
+
+int convertToPercent(int value)
+{
+  int percentValue = 0;
+  percentValue = map(value, 1023, 465, 0, 100);
+  return percentValue;
+}
+
+void printValues()
+{
+  Serial.print("\n\nAnalog Value: ");
+  Serial.print(sensorValue);
+  Serial.print("\nPercent: ");
+  Serial.print(percent);
+  Serial.print("%");
+}
